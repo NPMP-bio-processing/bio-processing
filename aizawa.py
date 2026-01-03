c84d9b297c0dce77071533cb60e02602336e6d14
Alen Leban <al2719@student.uni-lj.si>
2026-01-03 15:54:33 +0100

    Aizawa and brusselator with reactions

--- aizawa.py
+++ aizawa.py
@@ -4,18 +4,22 @@ from tqdm import tqdm
 import random
 
 
-class BrusselatorAttractor:
+class AizawaAttractor:
     """Brusselator - kaotični sistem"""
     
-    def __init__(self, a=1, b=1, gridx=5, gridy=5):
+    def __init__(self, a=0.92, b=0.7, c=0.67, d=3.5, e=0.25, f=0.1, gridx=5, gridy=5):
         self.a = a
         self.b = b
+        self.c = c
+        self.d = d
+        self.e = e
+        self.f = f
         self.gridx = gridx
         self.gridy = gridy
     
-    def solve(self, initial_state=[1.0, 1.0], t_end=50, n_points=2000):
+    def solve(self, initial_state=[0.1, 0.0, 0.0], t_end=50, n_points=2000):
         """Reši sistem z Tellurium"""
-        x0, y0 = initial_state
+        x0, y0, z0 = initial_state
         
         
         # model.integrator = 'gillespie'
@@ -23,20 +27,17 @@ class BrusselatorAttractor:
         solutions = []
         ts = []
         for r in tqdm(range(self.gridx * self.gridy)):
-            x0, y0 = initial_state
-            x0 += (r//self.gridy) * 0.2
-            y0 += (r%self.gridy) * 0.2
+            x0, y0, z0 = initial_state
+            # x0 += (r//self.gridy) * 0.2
+            # y0 += (r%self.gridy) * 0.2
             model = te.loada(f'''
-            model brusselator
+            model aizawa
+                x' = (z - b)*x - d*y
+                y' = d*x + (z - b)*y
+                z' = c + a*z - z*z*z/3 - x*x + f*z*x*x*x
                 
-                $A -> X;         A
-                2 X + Y -> 3 X; X*X*Y
-                $B + X -> Y + D; B*X
-                X -> ;         X
-                
-
-                A = {self.a}; B = {self.b};
-                X = {x0}; Y = {y0}; 
+                a = {self.a}; b = {self.b}; c = {self.c}; d = {self.d}; e = {self.e}; f = {self.f};
+                x = {x0}; y = {y0}; z = {z0};
             end
         ''')
             result = model.simulate(0, t_end, n_points)
@@ -49,23 +50,24 @@ class BrusselatorAttractor:
     def plot_3d(self, solutions):
         """3D vizualizacija"""
         fig = plt.figure(figsize=(10, 8))
-        ax = fig.add_subplot(111)
+        ax = fig.add_subplot(111, projection='3d')
         #print(solutions.shape)
         # colors = [((1 - i/len(solutions[0]))**2,0.5, (i/len(solutions[0]))**0.5) for i in range(len(solutions[0])) ]
         for solution in solutions:
             #print(type(solutions))
-            ax.plot(solution[:, 0], solution[:, 1], 
-                    linewidth=0.5, alpha=0.3, color="tab:blue")
+            ax.scatter(solution[:, 0], solution[:, 2], solution[:, 1], 
+                    linewidth=0.5, s=1, alpha=0.3, color="tab:blue")
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
-        ax.set_title('Brusselator atraktor')
+        ax.set_zlabel('Z')
+        ax.set_title('Aizawa atraktor')
         plt.tight_layout()
         plt.show()
     
     def plot_time_series(self, t, solution):
         """Časovni potek"""
-        fig, axes = plt.subplots(2, 1, figsize=(10, 6))
-        labels = ['X', 'Y']
+        fig, axes = plt.subplots(3, 1, figsize=(10, 6))
+        labels = ['X', 'Y', 'Z']
         for i, (ax, label) in enumerate(zip(axes, labels)):
             ax.plot(t, solution[:, i])
             ax.set_ylabel(label)
@@ -76,10 +78,10 @@ class BrusselatorAttractor:
 
 
 if __name__ == "__main__":
-    print("Brusselator atraktor - simulacija")
+    print("Aizawa atraktor - simulacija")
     
-    brusselator = BrusselatorAttractor(a=1, b=3, gridx=5, gridy=5)
-    ts, solutions = brusselator.solve(t_end=20, initial_state=[0.5, 1.0], n_points=500)
+    brusselator = AizawaAttractor(gridx=1, gridy=1)
+    ts, solutions = brusselator.solve(t_end=1000, n_points=200000)
     
     print(f"Simulirano {len(ts)} točk")
     brusselator.plot_3d(solutions)
--- brusselator.py
+++ brusselator.py
@@ -7,12 +7,13 @@ import random
 class BrusselatorAttractor:
     """Brusselator - kaotični sistem"""
     
-    def __init__(self, a=1, b=1, runs=5):
+    def __init__(self, a=1, b=1, gridx=5, gridy=5):
         self.a = a
         self.b = b
-        self.runs = runs
+        self.gridx = gridx
+        self.gridy = gridy
     
-    def solve(self, initial_state=[1.0, 1.0], t_end=50, n_points=5000):
+    def solve(self, initial_state=[1.0, 1.0], t_end=50, n_points=2000):
         """Reši sistem z Tellurium"""
         x0, y0 = initial_state
         
@@ -21,10 +22,10 @@ class BrusselatorAttractor:
         # model.integrator.seed = 1234
         solutions = []
         ts = []
-        for r in tqdm(range(self.runs)):
+        for r in tqdm(range(self.gridx * self.gridy)):
             x0, y0 = initial_state
-            x0 *= 1 + random.random()*2
-            y0 *= 1 + random.random()*2
+            x0 += (r//self.gridy) * 0.2
+            y0 += (r%self.gridy) * 0.2
             model = te.loada(f'''
             model brusselator
                 x' = {self.a} + x * x * y - {self.b} * x - x
@@ -45,10 +46,11 @@ class BrusselatorAttractor:
         fig = plt.figure(figsize=(10, 8))
         ax = fig.add_subplot(111)
         #print(solutions.shape)
+        # colors = [((1 - i/len(solutions[0]))**2,0.5, (i/len(solutions[0]))**0.5) for i in range(len(solutions[0])) ]
         for solution in solutions:
             #print(type(solutions))
             ax.plot(solution[:, 0], solution[:, 1], 
-                    linewidth=0.5, alpha=0.5, color="tab:blue")
+                    linewidth=0.5, alpha=0.3, color="tab:blue")
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.set_title('Brusselator atraktor')
@@ -71,8 +73,8 @@ class BrusselatorAttractor:
 if __name__ == "__main__":
     print("Brusselator atraktor - simulacija")
     
-    brusselator = BrusselatorAttractor(a=1, b=3, runs=50)
-    ts, solutions = brusselator.solve(t_end=60)
+    brusselator = BrusselatorAttractor(a=1, b=3, gridx=10, gridy=16)
+    ts, solutions = brusselator.solve(t_end=20, initial_state=[0.5, 1.0], n_points=500)
     
     print(f"Simulirano {len(ts)} točk")
     brusselator.plot_3d(solutions)
